13a68b952051cc894c709338949458fc7fdad427
jay <jayfreemandev@gmail.com>
2023-08-16 17:08:05 +0900

    :sparkles: : Gateway Service 추가

--- Gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,13 +2,12 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaServer
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
