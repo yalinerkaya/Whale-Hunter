cfeecea495f08f2f288e94ab05baece082e3c380
jay <jayfreemandev@gmail.com>
2023-08-16 17:07:23 +0900

    :sparkles: : Eureka Service Discovery 추가

--- WhaleHunter/src/main/java/com/example/whalehunter/domain/track/application/TrackService.java
+++
@@ -1,9 +0,0 @@
-package com.example.whalehunter.domain.track.application;
-
-import com.example.whalehunter.domain.track.domain.CoinData;
-
-import java.util.List;
-
-public interface TrackService {
-    List<CoinData> selectTop3coins() throws Exception;
-}
