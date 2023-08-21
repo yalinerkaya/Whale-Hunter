bc85ca68a3324292e2d71c7fc059d984bd89693c
jay <jayfreemandev@gmail.com>
2023-08-21 15:13:18 +0900

    :recycle: : Entity 조회 -> DTO 조회로 수정

--- Message/src/main/java/com/example/message/dao/CoinRepository.java
+++ Message/src/main/java/com/example/message/dao/CoinRepository.java
@@ -1,7 +1,10 @@
 package com.example.message.dao;
 
 import com.example.message.domain.Coin;
+import com.example.message.dto.CoinStatusResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -18,4 +21,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CoinRepository extends JpaRepository<Coin, Long> {
     Coin findBySymbol(String symbol);
+    @Query("SELECT c.status FROM Coin c WHERE c.symbol = :symbol")
+    String findStatusBySymbol(@Param("symbol") String symbol);
 }
--- Track/src/main/java/com/example/track/dao/MoveAverageRepository.java
+++ Track/src/main/java/com/example/track/dao/MoveAverageRepository.java
@@ -1,21 +1,25 @@
 package com.example.track.dao;
 
 import com.example.track.domain.MoveAverage;
+import com.example.track.dto.MoveAverageResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * packageName    : com.example.track.dao
  * fileName       : MoveAverageRepository
- * author         : 정재윤
+ * author         : Jay
  * date           : 2023-07-24
  * description    :
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
- * 2023-07-24        정재윤       최초 생성
+ * 2023-07-24        Jay       최초 생성
  */
 @Repository
 public interface MoveAverageRepository extends JpaRepository<MoveAverage, Long> {
-    MoveAverage findTopByOrderByCreatedAtDesc();
+
+    MoveAverageResponse findTopByOrderByCreatedAtDesc();
 }
