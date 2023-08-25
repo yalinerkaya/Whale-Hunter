97bdb147a66c453384bf7191ef61d73bb6105924
jay <jayfreemandev@gmail.com>
2023-08-25 21:30:21 +0900

    ♻️ : Eureka Client 제거

--- Message/src/main/java/com/example/message/MessageApplication.java
+++ Message/src/main/java/com/example/message/MessageApplication.java
@@ -11,7 +11,6 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.message", "com.example.global.config"})
 public class MessageApplication {
--- Track/src/main/java/com/example/track/TrackApplication.java
+++ Track/src/main/java/com/example/track/TrackApplication.java
@@ -26,7 +26,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-@EnableEurekaClient
+
 @EnableScheduling
 @EnableCaching
 @EnableAsync
