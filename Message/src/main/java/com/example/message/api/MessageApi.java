cc0932845df3968b75a166ee08c548ab805304c5
jay <jayfreemandev@gmail.com>
2023-08-31 16:21:43 +0900

    :recycle: 메세지 api 미사용 로직 제거

--- Message/src/main/java/com/example/message/api/MessageApi.java
+++ Message/src/main/java/com/example/message/api/MessageApi.java
@@ -2,15 +2,14 @@ package com.example.message.api;
 
 import com.example.global.common.CommonResponse;
 import com.example.message.application.MessageService;
-import com.example.message.dto.CoinStatusResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/message")
 public class MessageApi {
     private final MessageService messageService;
 
